1721a6bf311a6a0987eee442e7bd4bc1448b10cf
lixianx xing <lixian666@mail.ynu.edu.cn>
2023-05-03 05:00:11 +0000

    add Json、utf_8 encoding and decoding

--- 14_decodingAndEncodingAndJson.dart
+++ 14_decodingAndEncodingAndJson.dart
@@ -1,12 +1,27 @@
 import 'dart:convert';
 
 void main() {
+  //NOTICE：Json对象中的字符串使用双引号
   var jsonString = '''
 [
-  {'姓名':'蔡徐坤'},
-  {'爱好':'篮球'}
+  {"姓名":"蔡徐坤"},
+  {"爱好":"篮球"}
 ]
 ''';
   var messege = jsonDecode(jsonString);
   print(messege);
+  print(messege[0]["姓名"]);
+  //Json编码
+  var scores = [
+    {'score': 40},
+    {'score': 60},
+    {
+      'score': [
+        {'math': 70},
+        {'english': 60}
+      ]
+    }
+  ];
+  var jsonText = jsonEncode(scores);
+  print(jsonText);
 }
